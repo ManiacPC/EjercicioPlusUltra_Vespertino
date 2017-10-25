b1ddc59313e2b84821777b2cbfdaa1fa8e532a46
Enrique Palacios <enriquepalaciosc@gmail.com>
2017-10-24 21:59:40 -0300

    CRUD terminado, incluyendo paso de objetos por intento

---
+++ app/src/main/java/cl/inacap/ejercicioplusultra/EditEquipoActivity.java
@@ -0,0 +1,13 @@
+package cl.inacap.ejercicioplusultra;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditEquipoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_equipo);
+    }
+}
